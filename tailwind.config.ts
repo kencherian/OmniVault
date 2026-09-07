69f9cb209d31ced70e4e86dbe7b0b9118a2f8a86
Ken <kencherian05@gmail.com>
2026-09-07 14:21:11 +0530

    tailwind config files

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#EA6365",
+          100: "#EA6365",
+        },
+        light: {
+          100: "#333F4E",
+          200: "#A3B2C7",
+          300: "#F2F5F9",
+          400: "#F2F4F8",
+        },
+        dark: {
+          100: "#04050C",
+          200: "#131524",
+        },
+        error: "#b80000",
+        green: "#31B66F",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
